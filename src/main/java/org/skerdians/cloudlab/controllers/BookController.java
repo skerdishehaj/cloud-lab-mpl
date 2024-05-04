44687258938f26f40898c58d4627a4a7e074197b
Skerdi Shehaj <skerdianshehaj@gmail.com>
2024-05-04 14:00:08 +0200

    Added logs using log4j2 dependncy starter from springboot

--- src/main/java/org/skerdians/cloudlab/controllers/BookController.java
+++ src/main/java/org/skerdians/cloudlab/controllers/BookController.java
@@ -1,5 +1,7 @@
 package org.skerdians.cloudlab.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.skerdians.cloudlab.entities.Book;
 import org.skerdians.cloudlab.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
+    private static final Logger LOGGER = LogManager.getLogger(BookController.class);
     private final BookService bookService;
 
     @Autowired
@@ -22,48 +25,71 @@ public class BookController {
 
     @PostMapping
     public ResponseEntity<Book> addBook(@RequestBody Book book) {
+        LOGGER.info("{}: Adding book: {}", BookController.class.getName(), book);
         Book addedBook = bookService.addBook(book);
+        LOGGER.info("{}: Book added: {}", BookController.class.getName(), addedBook);
+        LOGGER.debug("{}: Book added: {}", BookController.class.getName(), addedBook);
         return new ResponseEntity<>(addedBook, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<Collection<Book>> getAllBooks() {
+        LOGGER.info("{}: Getting all books", BookController.class.getName());
         Collection<Book> allBooks = bookService.getAllBooks();
+        LOGGER.info("{}: All books: {}", BookController.class.getName(), allBooks);
+        LOGGER.debug("{}: All books: {}", BookController.class.getName(), allBooks);
         return new ResponseEntity<>(allBooks, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Book> getBookWithId(@PathVariable Long id) {
+        LOGGER.info("{}: Getting book with id: {}", BookController.class.getName(), id);
         Optional<Book> bookById = bookService.getBookById(id);
+        LOGGER.info("{}: Book with id {}: {}", BookController.class.getName(), id, bookById);
+        LOGGER.debug("{}: Book with id {}: {}", BookController.class.getName(), id, bookById);
         return bookById.map(book -> new ResponseEntity<>(book, HttpStatus.OK))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping(params = {"name"})
     public ResponseEntity<Collection<Book>> findBookWithName(@RequestParam(value = "name") String name) {
+        LOGGER.info("{}: Finding book with name: {}", BookController.class.getName(), name);
         Collection<Book> booksByName = bookService.findBookByName(name);
+        LOGGER.info("{}: Books with name {}: {}", BookController.class.getName(), name, booksByName);
+        LOGGER.debug("{}: Books with name {}: {}", BookController.class.getName(), name, booksByName);
         return new ResponseEntity<>(booksByName, HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Book> updateBookFromDB(@PathVariable("id") long id, @RequestBody Book book) {
+        LOGGER.info("{}: Updating book with id: {}", BookController.class.getName(), id);
         Book updatedBook = bookService.updateBook(id, book);
         if (updatedBook != null) {
+            LOGGER.info("{}: Book with id {} updated: {}", BookController.class.getName(), id, updatedBook);
+            LOGGER.debug("{}: Book with id {} updated: {}", BookController.class.getName(), id, updatedBook);
             return new ResponseEntity<>(updatedBook, HttpStatus.OK);
         } else {
+            LOGGER.error("{}: Book with id {} not found", BookController.class.getName(), id);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteBookWithId(@PathVariable Long id) {
+        LOGGER.warn("{}: Deleting book with id: {}", BookController.class.getName(), id);
+        LOGGER.info("{}: Deleting book with id: {}", BookController.class.getName(), id);
         bookService.deleteBookById(id);
+        LOGGER.info("{}: Book with id {} deleted", BookController.class.getName(), id);
+        LOGGER.debug("{}: Book with id {} deleted", BookController.class.getName(), id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping
     public ResponseEntity<Void> deleteAllBooks() {
+        LOGGER.warn("{}: Deleting all books", BookController.class.getName());
         bookService.deleteAllBooks();
+        LOGGER.info("{}: All books deleted", BookController.class.getName());
+        LOGGER.debug("{}: All books deleted", BookController.class.getName());
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/org/skerdians/cloudlab/services/BookService.java
+++ src/main/java/org/skerdians/cloudlab/services/BookService.java
@@ -1,5 +1,7 @@
 package org.skerdians.cloudlab.services;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.skerdians.cloudlab.entities.Book;
 import org.skerdians.cloudlab.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,48 +12,64 @@ import java.util.Optional;
 
 @Service
 public class BookService {
+    public static Logger LOGGER = LogManager.getLogger(BookService.class);
     private final BookRepository bookRepository;
 
     @Autowired
     public BookService(BookRepository bookRepository) {
+        LOGGER.info("Creating BookService");
         this.bookRepository = bookRepository;
     }
 
     public Book addBook(Book book) {
+        // First param should be  but it is not working
+        LOGGER.info("{}: Adding book: {}", BookService.class.getName(), book);
         return bookRepository.save(book);
     }
 
     public Collection<Book> getAllBooks() {
+        LOGGER.info("{}: Getting all books", BookService.class.getName());
         return bookRepository.findAll();
     }
 
     public Optional<Book> getBookById(Long id) {
+        LOGGER.info("{}: Getting book with id: {}", BookService.class.getName(), id);
         return bookRepository.findById(id);
     }
 
     public Collection<Book> findBookByName(String name) {
+        LOGGER.info("{}: Finding book with name: {}", BookService.class.getName(), name);
         return bookRepository.findByName(name);
     }
 
     public Book updateBook(Long id, Book book) {
+        LOGGER.info("{}: Updating book with id: {}", BookService.class.getName(), id);
         Optional<Book> currentBookOpt = bookRepository.findById(id);
         if (currentBookOpt.isPresent()) {
+            LOGGER.info("{}: Book found: {}", BookService.class.getName(), currentBookOpt.get());
             Book currentBook = currentBookOpt.get();
             currentBook.setName(book.getName());
             currentBook.setDescription(book.getDescription());
             currentBook.setTagsList(book.getTagsList());
+            LOGGER.info("{}: Book updated: {}", BookService.class.getName(), currentBook);
             return bookRepository.save(currentBook);
         } else {
+            LOGGER.info("{}: Book not found with id: {}", BookService.class.getName(), id);
+            LOGGER.warn("{}: Book not found with id: {}", BookService.class.getName(), id);
             // Handle not found scenario
             return null;
         }
     }
 
     public void deleteBookById(Long id) {
+        LOGGER.info("{}: Deleting book with id: {}", BookService.class.getName(), id);
+        LOGGER.warn("{}: Deleting book with id: {}", BookService.class.getName(), id);
         bookRepository.deleteById(id);
     }
 
     public void deleteAllBooks() {
+        LOGGER.info("{}: Deleting all books", BookService.class.getName());
+        LOGGER.warn("{}: Deleting all books", BookService.class.getName());
         bookRepository.deleteAll();
     }
 }
